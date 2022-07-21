fe0a1a3f14c35560930df11be0fdd649d7fd3ec3
James Wu <ichangjwu@outlook.com>
2022-07-22 01:35:52 +0900

    Add grid to game board

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,3 +1,9 @@
 function GameBoard() {
-  
+  let grid = new Array(10).fill(null).map(() => new Array(10).fill(null));
+
+  return {
+    getGrid: () => grid,
+  }
 }
+
+export default GameBoard;
---
+++ tests/gameboard.test.js
@@ -0,0 +1,24 @@
+import GameBoard from '../src/gameBoard'
+
+describe('GameBoard factory', () => {
+  let gameBoard;
+
+  beforeEach(() => {
+    gameBoard = GameBoard();
+  });
+
+  it('creates a game board with a grid', () => {
+    expect(gameBoard.getGrid()).toEqual([
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null]
+    ]);
+  });
+});
