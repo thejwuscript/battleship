d7fef655bf6141e67ca3de59cd478388cead2f30
James Wu <ichangjwu@outlook.com>
2022-07-23 02:02:05 +0900

    Setup UI

---
+++ src/domInteraction.js
@@ -0,0 +1,15 @@
+const domInteraction = (() => {
+  // select the div element with the class of "human-board"
+  // select the div element with the class of "ai-board"
+  // get the game board grid from each player and store them in a variable separately
+  // flatten the multidimensional array into a single array with 100 elements and store that into a variable
+  // element will be either null or a string
+  // iterate through the array
+  // for each element, create a new div element
+  // add the class "coordinate" to the div element
+  // if the element is null, add a class of 'empty' to the div
+  // if the element is a string, add a class of 'occupied' to the div
+  // append the div element to each respective player's div
+
+  return {}
+})();
--- src/game.js
+++ src/game.js
@@ -1,4 +1,20 @@
+import AIPlayer from '../src/aiPlayer';
+import GameBoard from '../src/gameBoard';
+import Player from '../src/player';
+
 const game = (() => {
-  
+  const humanBoard = GameBoard();
+  const aiBoard = GameBoard();
+  const humanPlayer = Player(humanBoard);
+  const aiPlayer = AIPlayer(aiBoard);
+
+  Object.values(humanBoard.getShips()).forEach((ship, index) => {
+    humanBoard.placeShip(ship, [index, index], 'horizontal');
+  })
+
+  Object.values(aiBoard.getShips()).forEach((ship, index) => {
+    aiBoard.placeShip(ship, [index, index], 'vertical');
+  });
+
   return {}
 })();
