620c8579458386bf4ec90f02f8ec57026f7c5ea0
James Wu <ichangjwu@outlook.com>
2022-07-23 02:49:14 +0900

    Add data-location attribute

--- src/domInteraction.js
+++ src/domInteraction.js
@@ -1,28 +1,19 @@
 const domInteraction = (() => {
   const displayGrid = (grid, container) => {
-    const flattenedGrid = grid.flat();
-    flattenedGrid.forEach(cell => {
-      const coordinateDiv = document.createElement('div');
-      coordinateDiv.classList.add('coordinate');
-      if (cell === null) {
-        coordinateDiv.classList.add('empty');
-      } else {
-        coordinateDiv.classList.add('occupied');
-      }
-      container.appendChild(coordinateDiv);
-    })
-  }
-  // select the div element with the class of "human-board"
-  // select the div element with the class of "ai-board"
-  // get the game board grid from each player and store them in a variable separately
-  // flatten the multidimensional array into a single array with 100 elements and store that into a variable
-  // element will be either null or a string
-  // iterate through the array
-  // for each element, create a new div element
-  // add the class "coordinate" to the div element
-  // if the element is null, add a class of 'empty' to the div
-  // if the element is a string, add a class of 'occupied' to the div
-  // append the div element to each respective player's div
+    grid.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        const coordinateDiv = document.createElement('div');
+        coordinateDiv.classList.add('coordinate');
+        coordinateDiv.dataset.location = `${rowIndex},${colIndex}`;
+        if (cell === null) {
+          coordinateDiv.classList.add('empty');
+        } else {
+          coordinateDiv.classList.add('occupied');
+        }
+        container.appendChild(coordinateDiv);
+      });
+    });
+  };
 
   return { displayGrid }
 })();
